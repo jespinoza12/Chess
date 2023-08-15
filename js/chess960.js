6498fff67904a9bb81e4d9f81c5e9c0156096155
jespinoza12 <jespinoza@student.neumont.edu>
2023-08-14 22:05:45 -0600

    Rook Logic

--- js/chess960.js
+++ js/chess960.js
@@ -1,20 +1,45 @@
 const start960ID = document.getElementById("start960");
 
-
 const randomizePieces = () => {
-    reset();
-  
-    let starterPieces = ["p", "p", "p", "p", "p", "p", "p", "p"];
-    let rooksAdded = rookLogic(starterPieces);
-    let kingAdded = kingLogic(rooksAdded);
-    let bishopsAdded = bishopLogic(kingAdded);
-    let allPiecesAdded = restOfPiecesLogic(bishopsAdded);
-    let blackRanks = allPiecesAdded.join().toString().replaceAll(",", "");
-    let whiteRanks = blackRanks.toUpperCase();
-  
-    game.load(
-      `${blackRanks}/pppppppp/8/8/8/8/PPPPPPPP/${whiteRanks} w KQkq - 0 1`
-    );
-    board.position(game.fen());
-  };
-  
+  reset();
+
+  let starterPieces = ["p", "p", "p", "p", "p", "p", "p", "p"];
+  let rooksAdded = rookLogic(starterPieces);
+  let kingAdded = kingLogic(rooksAdded);
+  let bishopsAdded = bishopLogic(kingAdded);
+  let allPiecesAdded = restOfPiecesLogic(bishopsAdded);
+  let blackRanks = allPiecesAdded.join().toString().replaceAll(",", "");
+  let whiteRanks = blackRanks.toUpperCase();
+
+  game.load(
+    `${blackRanks}/pppppppp/8/8/8/8/PPPPPPPP/${whiteRanks} w KQkq - 0 1`
+  );
+  board.position(game.fen());
+};
+
+const rookLogic = (rankPieces) => {
+  let firstRookPosition = getRandomInt(8);
+  let secondRookPosition = getRandomInt(firstRookPosition);
+  let incorrectPosition = true;
+
+  while (incorrectPosition) {
+    if (
+      firstRookPosition === secondRookPosition ||
+      firstRookPosition - 1 === secondRookPosition
+    ) {
+      firstRookPosition = getRandomInt(8);
+      secondRookPosition = getRandomInt(firstRookPosition);
+    } else {
+      incorrectPosition = false;
+    }
+  }
+
+  let newArr = [...rankPieces];
+  newArr.forEach((p, index) => {
+    if (index === firstRookPosition || index === secondRookPosition) {
+      newArr[index] = "r";
+    }
+  });
+
+  return newArr;
+};
