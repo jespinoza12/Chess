58fad1691af9e34cb5469a1a66259d996c180a53
jespinoza12 <jespinoza@student.neumont.edu>
2023-08-14 22:08:21 -0600

    Shuffle

--- js/chess960.js
+++ js/chess960.js
@@ -1,4 +1,4 @@
-const start960ID = document.getElementById("start960");
+const chess960 = document.getElementById("chess960");
 
 const randomizePieces = () => {
   reset();
@@ -45,58 +45,84 @@ const rookLogic = (rankPieces) => {
 };
 
 const kingLogic = (rankPieces) => {
-    let rookIndexLocations = [];
-    rankPieces.forEach((v, index) => {
-      if (v === "r") {
-        rookIndexLocations.push(index);
-      }
-    });
-  
-    let kingPosition = getRandomIntRange(
-      rookIndexLocations[0] + 1,
-      rookIndexLocations[1]
-    );
-  
-    rankPieces[kingPosition] = "k";
-    return rankPieces;
-  };
-  
-  const bishopLogic = (rankPieces) => {
-    let emptyOddLocations = [];
-    let emptyEvenLocations = [];
-    rankPieces.forEach((v, index) => {
-      if (v === "p" && index % 2 == 0) {
-        emptyEvenLocations.push(index);
-      }
-      if (v === "p" && index % 2 == 1) {
-        emptyOddLocations.push(index);
-      }
-    });
-  
-    let firstBishop = getRandomInt(emptyOddLocations.length);
-    let secondBishop = getRandomInt(emptyEvenLocations.length);
-    let firstBishopIndex = emptyOddLocations[firstBishop];
-    let secondBishopIndex = emptyEvenLocations[secondBishop];
-    rankPieces[firstBishopIndex] = "b";
-    rankPieces[secondBishopIndex] = "b";
-  
-    return rankPieces;
-  };
+  let rookIndexLocations = [];
+  rankPieces.forEach((v, index) => {
+    if (v === "r") {
+      rookIndexLocations.push(index);
+    }
+  });
 
+  let kingPosition = getRandomIntRange(
+    rookIndexLocations[0] + 1,
+    rookIndexLocations[1]
+  );
+
+  rankPieces[kingPosition] = "k";
+  return rankPieces;
+};
+
+const bishopLogic = (rankPieces) => {
+  let emptyOddLocations = [];
+  let emptyEvenLocations = [];
+  rankPieces.forEach((v, index) => {
+    if (v === "p" && index % 2 == 0) {
+      emptyEvenLocations.push(index);
+    }
+    if (v === "p" && index % 2 == 1) {
+      emptyOddLocations.push(index);
+    }
+  });
+
+  let firstBishop = getRandomInt(emptyOddLocations.length);
+  let secondBishop = getRandomInt(emptyEvenLocations.length);
+  let firstBishopIndex = emptyOddLocations[firstBishop];
+  let secondBishopIndex = emptyEvenLocations[secondBishop];
+  rankPieces[firstBishopIndex] = "b";
+  rankPieces[secondBishopIndex] = "b";
+
+  return rankPieces;
+};
 
 const restOfPiecesLogic = (rankPieces) => {
-    let emptyLocations = [];
-    rankPieces.forEach((v, index) => {
-      if (v === "p") {
-        emptyLocations.push(index);
-      }
-    });
-    let shuffledLocations = shuffle(emptyLocations);
-    rankPieces[shuffledLocations[0]] = "q";
-    rankPieces[shuffledLocations[1]] = "n";
-    rankPieces[shuffledLocations[2]] = "n";
-  
-    return rankPieces;
-  };
+  let emptyLocations = [];
+  rankPieces.forEach((v, index) => {
+    if (v === "p") {
+      emptyLocations.push(index);
+    }
+  });
+  let shuffledLocations = shuffle(emptyLocations);
+  rankPieces[shuffledLocations[0]] = "q";
+  rankPieces[shuffledLocations[1]] = "n";
+  rankPieces[shuffledLocations[2]] = "n";
 
-  
+  return rankPieces;
+};
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const getRandomIntRange = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+chess960.addEventListener("click", randomizePieces);
